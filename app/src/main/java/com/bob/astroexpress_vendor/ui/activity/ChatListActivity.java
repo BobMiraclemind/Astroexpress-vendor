73159afae5248fdf7cf1fedc0fb9ad73e6ac8e4d
BobMiraclemind <BobMiraclemind>
2022-01-05 17:39:09 +0530

    Initial Commit

--- app/src/main/java/com/bob/astroexpress_vendor/ui/activity/ChatListActivity.java
+++ app/src/main/java/com/bob/astroexpress_vendor/ui/activity/ChatListActivity.java
@@ -1,14 +1,21 @@
 package com.bob.astroexpress_vendor.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 
+import com.bob.astroexpress_vendor.adapter.UserListAdapter;
 import com.bob.astroexpress_vendor.databinding.ActivityChatListBinding;
+import com.bob.astroexpress_vendor.datamodel.UsersModel;
+
+import java.util.ArrayList;
 
 public class ChatListActivity extends AppCompatActivity {
 
     ActivityChatListBinding binding;
+    UserListAdapter adapter;
+    ArrayList<UsersModel> dataHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,11 @@ public class ChatListActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         getSupportActionBar().setTitle("Available Users");
+
+        dataHolder = new ArrayList<>();
+
+        adapter = new UserListAdapter(getApplicationContext(),dataHolder);
+        binding.recview.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
 
     }
 }
